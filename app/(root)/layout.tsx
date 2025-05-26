f9474243390231f2e2b472a32bb685743029a47c
Zack Honey <zhoney466@gmail.com>
2025-05-25 20:09:00 -0700

    wip

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,13 @@
 import Navbar from "../components/Navbar";
+import { SessionProvider } from "next-auth/react";
 
-export default function Layout({ children }: Readonly<{ children: React.ReactNode}>) {
+export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <main className="font-work-sans">
-            <Navbar />
-    
-            {children}
+            <SessionProvider>
+                <Navbar />
+                {children}
+            </SessionProvider>
         </main>
     )
 }
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+"use client";
+
+import { signOut, signIn } from '@/auth'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useSession } from 'next-auth/react';
+// Import your session hook here, e.g. useSession from next-auth/react
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    // Replace with your actual session hook
+    const { data: session } = useSession();
+
+    return (
+        <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
+            <nav className='flex justify-between items-center'>
+                <Link href='/'>
+                    <Image src='/logo.png' alt='logo' width={144} height={30} />
+                </Link>
+
+                <div className='flex items-center gap-5'>
+                    {session && session?.user ? (
+                        <>
+                            <Link href='/startup/create'>
+                                <span>Create</span>
+                            </Link>
+
+                            <button onClick={() => signOut()}>
+                                <span>Logout</span>
+                            </button>
+
+                            <Link href={`/user/${session?.id}`}>
+                                <span>{session?.user?.name}</span>
+                            </Link>
+                        </>
+                    ) : (
+                        <button onClick={() => signIn('github')}>
+                            <span>Login</span>
+                        </button>
+                    )}
+                </div>
+            </nav>
+        </header>
+    )
 }
 
 export default Navbar
