9aab2a8d69baa261d4a82232f3dbda4d596ec0b3
Zack Honey <zhoney466@gmail.com>
2025-05-25 20:39:51 -0700

    wip

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,11 @@
 import Navbar from "../components/Navbar";
-import { SessionProvider } from "next-auth/react";
 
-export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default function Layout({ children }: Readonly<{ children: React.ReactNode}>) {
     return (
         <main className="font-work-sans">
-            <SessionProvider>
-                <Navbar />
-                {children}
-            </SessionProvider>
+            <Navbar />
+    
+            {children}
         </main>
     )
 }
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,9 @@
-"use client";
-
-import { signOut, signIn } from '@/auth'
+import { auth, signOut, signIn } from '@/auth'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useSession } from 'next-auth/react';
-// Import your session hook here, e.g. useSession from next-auth/react
 
-const Navbar = () => {
-    // Replace with your actual session hook
-    const { data: session } = useSession();
+const Navbar = async () => {
+    const session = await auth();
 
     return (
         <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
@@ -17,25 +12,35 @@ const Navbar = () => {
                     <Image src='/logo.png' alt='logo' width={144} height={30} />
                 </Link>
 
-                <div className='flex items-center gap-5'>
+                <div className='flex items-center gap-5 text-black'>
                     {session && session?.user ? (
                         <>
                             <Link href='/startup/create'>
                                 <span>Create</span>
                             </Link>
 
-                            <button onClick={() => signOut()}>
-                                <span>Logout</span>
-                            </button>
+                            <form action={async () => {
+                                "use server"
+                                await signOut({ redirectTo: '/' });
+                            }}>
+                                <button>
+                                    Logout
+                                </button>
+                            </form>
 
                             <Link href={`/user/${session?.id}`}>
                                 <span>{session?.user?.name}</span>
                             </Link>
                         </>
                     ) : (
-                        <button onClick={() => signIn('github')}>
-                            <span>Login</span>
-                        </button>
+                        <form action={async () => {
+                            'use server'
+                            await signIn('github');
+                        }}>
+                            <button type="submit">
+                                Login
+                            </button>
+                        </form>
                     )}
                 </div>
             </nav>
