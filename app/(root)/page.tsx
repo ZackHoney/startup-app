ff952936ee5b4091535a748718c1b0011706ab75
Zack Honey <zhoney466@gmail.com>
2025-05-26 19:19:44 -0700

    wip

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,18 @@
 export default function Home() {
   return (
     <>
-      <h1 className="heading">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect With Entrepeneurs
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Notice in Virtual Competitions.
+        </p>
+
+      
+
+      </section>
     </>
   );
 }
